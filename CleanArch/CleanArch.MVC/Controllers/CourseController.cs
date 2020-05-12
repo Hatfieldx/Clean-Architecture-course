cfbd83140a2a37e1209214de2f6aac8a5d533178
hatfieldx <tsyganov.nn@gmail.com>
2020-05-13 01:02:05 +0700

    Change present layer

---
+++ CleanArch/CleanArch.MVC/Controllers/CourseController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Domain.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.MVC.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+        
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        [Authorize]
+        public IActionResult Index()
+        {
+            var model = _courseService.GetCourses();
+
+            return View(model);
+        }
+    }
+}
