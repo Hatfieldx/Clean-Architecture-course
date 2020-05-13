fb8554f898fdc25c1afc8485c94d24736687ba4d
hatfieldx <tsyganov.nn@gmail.com>
2020-05-13 17:00:09 +0700

    added default api projec

--- CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
+++ CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
@@ -1,10 +1,12 @@
 ﻿using CleanArch.Application.ViewModels;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CleanArch.Application.Interfaces
 {
     public interface ICourseService
     {
         CourseViewModel GetCourses();
+        void CreateCourse(CourseViewModel courseViewModel);
     }
 }
--- CleanArch/CleanArch.Application/Services/CourseService.cs
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -1,17 +1,28 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
+using System.Threading.Tasks;
 
 namespace CleanArch.Application.Services
 {
     public class CourseService : ICourseService
     {
-        private ICourseRepository _courseRepository;
+        private readonly ICourseRepository _courseRepository;
+        private readonly IMediatorHandler _bus;
 
-        public CourseService(ICourseRepository courseRepository)
+        public CourseService(ICourseRepository courseRepository, IMediatorHandler bus)
         {
             _courseRepository = courseRepository;
+            _bus = bus;
         }
+
+        public void CreateCourse(CourseViewModel courseViewModel)
+        {
+            _bus.SendCommand(new CreateCourseCommand(courseViewModel.Name, courseViewModel.Description, courseViewModel.ImageUrl));
+        }
+
         public CourseViewModel GetCourses()
         {
             return new CourseViewModel { Courses = _courseRepository.GetCourses() };
